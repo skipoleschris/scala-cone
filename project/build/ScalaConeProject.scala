0424b96be4adc14dacf47e0c48c360fef0c95ed0
Chris Turner <chris@templemore.co.uk>
2011-04-05 13:35:41 +0100

    Initial check in of project files

---
+++ project/build/ScalaConeProject.scala
@@ -0,0 +1,24 @@
+import sbt._
+import reaktor.scct.ScctProject
+
+class ScalaConeProject(info: ProjectInfo) extends DefaultProject(info) with ScctProject {
+
+  // Additional repositories
+  val snapshots = "snapshots" at "http://scala-tools.org/repo-snapshots"
+  val releases  = "releases" at "http://scala-tools.org/repo-releases"
+
+  // Compile Dependencies
+  val slf4jApi = "org.slf4j" % "slf4j-api" % "1.6.1" % "compile"
+  val logback = "ch.qos.logback" % "logback-classic" % "0.9.28" % "compile"
+
+  // Test dependencies
+  val specs2 = "org.specs2" %% "specs2" % "1.1-SNAPSHOT"
+
+  // Spec2 configuration
+  def specs2Framework = new TestFramework("org.specs2.runner.SpecsFramework")
+  override def testFrameworks = super.testFrameworks ++ Seq(specs2Framework)
+}
+
+ 
+
+ 
